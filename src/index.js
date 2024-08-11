a6a09b539c3e94d722741f7a70eef0a692c57eac
Abhijit Singh <abhijitsingh.op25@gmail.com>
2024-08-11 14:26:54 +0530

    sql server setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-mkakaka
+const express = require("express");
+const { PORT } = require('./config/serverConfig.js')
+
+const startServer = () => {
+    const app = express();
+    
+    app.listen(PORT, () => {
+        console.log(`server running on port ${PORT}`);
+    })
+}
+
+startServer();
